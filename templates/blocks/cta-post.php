8735968c2261680aaae90a3bc36656ea974edb92
Sarah Marinoff <sarah@baytek.ca>
2020-06-22 15:32:37 -0400

    translate the cat block if needed

--- templates/blocks/cta-post.php
+++ templates/blocks/cta-post.php
@@ -1,8 +1,11 @@
 <?php
 	//Template for rendering the posts final CTA
+
+	//Get the translated ID, if applicable
+	$block_id = apply_filters('wpml_object_id', (int) get_option('post-settings-final-cta-block'), 'wp_block', true);
 ?>
 
-<?php if ($block = get_post(get_option('post-settings-final-cta-block'))) : ?>
+<?php if ($block = get_post($block_id)) : ?>
 	<section class="final-cta">
 		<?php echo apply_filters('the_content', $block->post_content); ?>
 	</section>

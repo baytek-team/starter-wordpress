f4340eea111269c8de684dd26fe912e1de3a3953
Sarah Marinoff <sarah@baytek.ca>
2020-11-13 10:11:54 -0500

    fixed final cat block so it won’t show random content when a final cat isn’t set

--- templates/blocks/cta-post.php
+++ templates/blocks/cta-post.php
@@ -3,9 +3,10 @@
 
 	//Get the translated ID, if applicable
 	$block_id = apply_filters('wpml_object_id', (int) get_option('post-settings-final-cta-block'), 'wp_block', true);
+	$block = get_post($block_id);
 ?>
 
-<?php if ($block = get_post($block_id)) : ?>
+<?php if ($block && $block_id && isset($block->post_content)) : ?>
 	<section class="final-cta">
 		<?php echo apply_filters('the_content', $block->post_content); ?>
 	</section>

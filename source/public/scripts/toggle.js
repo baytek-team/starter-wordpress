0ed21859d93e430171da5e57b036cd69fac5ac21
Chris Anderson <chris@baytek.ca>
2021-07-14 10:34:29 -0400

    removing all instances of color variables and replacing them with $black.  It's something we always have to go through and change when we start a new project.  Also updated the headings with individual line-heights and weights

--- source/public/scripts/toggle.js
+++ source/public/scripts/toggle.js
@@ -1,8 +1,11 @@
 // Toggle interactivity - use jQuery SlideToggle if needed
 (function($){
-	$(".wp-block-theme-blocks-toggle .toggle-header").on( "click", function() {
+	$(".wp-block-baytek-toggle .toggle-header").on( "click", function() {
 		$(this).next().slideToggle("300", function() {
 			$(this).parent().toggleClass("expanded");
 		});
 	});
+
+
 })(jQuery);
+
